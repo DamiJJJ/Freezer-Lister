e407901e7bbcfc3de005626a9d2d3d0531f283d3
DamiJJJ <damipl1280@outlook.com>
2023-08-06 19:31:49 +0200

    Create user

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,34 @@
 var Userdb = require("../model/model");
 
 // create and save new user
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  //   new user
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  //   save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creatin a create operation",
+      });
+    });
+};
 
 // retrieve and return all users/ retrieve and return a snigle user
 exports.find = (req, res) => {};
